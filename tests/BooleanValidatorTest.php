d77f0131dde3df91cf877c1daea4e00b79cf5624
Carole Carduner <carole.carduner@gmail.com>
2014-11-24 11:20:08 +0100

    Add BooleanValidatorTest/phpunit.xml.dist Modify composer

---
+++ tests/BooleanValidatorTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carole
+ * Date: 24/11/2014
+ * Time: 11:07
+ */
+
+namespace Tests\Carole_A3_Rendu\TypeValidator;
+use Carole_A3_Rendu\TypeValidator;
+
+
+/**
+ * Class BooleanValidatorTest
+ * @package Tests\Carole_A3_Rendu\TypeValidator
+ */
+class BooleanValidatorTest extends \PHPUnit_Framework_TestCase
+{
+
+}
